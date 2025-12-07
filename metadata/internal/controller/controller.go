a04afb6248245942de9847447f389e183335e767
“swathikrish753” <swathikrish753@gmail.com>
2025-12-07 19:14:30 +0530

    Added Controller

---
+++ metadata/internal/controller/controller.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"context"
+	"errors"
+
+	"movieexample.com/metadata/internal/repository"
+	model "movieexample.com/metadata/pkg"
+)
+
+var ErrNotFound = errors.New("not found")
+
+type metadataRepository interface {
+	Get(ctx context.Context, id string) (*model.Metadata, error)
+}
+
+type Controller struct {
+	repo metadataRepository
+}
+
+func New(repo metadataRepository) *Controller {
+	return &Controller{repo}
+}
+
+func (c *Controller) Get(ctx context.Context, id string) (*model.Metadata, error) {
+	res, err := c.repo.Get(ctx, id)
+	if err != nil && errors.Is(err, repository.ErrNotFound) {
+		return nil, ErrNotFound
+	}
+	return res, nil
+}
