8b6cbe1cbcb15e4f6f33e48648b504bac3b428b5
“swathikrish753” <swathikrish753@gmail.com>
2026-04-18 13:32:22 +0530

    Model Created

---
+++ metadata/pkg/metadata.go
@@ -0,0 +1,8 @@
+package model
+
+type Metadata struct {
+	ID          string `json:"id"`
+	Tile        string `json:"title"`
+	Description string `json:"description"`
+	Director    string `json:"director"`
+}
